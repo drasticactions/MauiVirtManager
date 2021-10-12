6699f30f45b00aa58fb946d3a825a7bf7694393a
Tim Miller <innerlogic4321@gmail.com>
2021-10-12 12:03:44 -0400

    Port Initial Tooling
    - AsyncCommand
    - IErrorHandlerService
    - Translations/Common.resx

---
+++ MauiVirtManager/Services/Implementations/ErrorHandlerService.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="ErrorHandlerService.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace MauiVirtManager.Services
+{
+    /// <summary>
+    /// Error Handler Service.
+    /// </summary>
+    public class ErrorHandlerService : IErrorHandlerService
+    {
+        private INavigationService navigation;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorHandlerService"/> class.
+        /// </summary>
+        /// <param name="navigation">Awful Navigation.</param>
+        public ErrorHandlerService(INavigationService navigation)
+        {
+            this.navigation = navigation;
+        }
+
+        /// <inheritdoc/>
+        public void HandleError(Exception exception)
+        {
+            if (exception == null)
+            {
+                return;
+            }
+
+            // TODO: Log exception to error handling service provider.
+            string errorMessage = string.Format(Translations.Common.ErrorMessage, exception.GetType().FullName, exception.Message, exception.StackTrace);
+
+            System.Diagnostics.Debug.WriteLine(errorMessage);
+            this.navigation.DisplayAlertAsync(Translations.Common.ErrorTitle, errorMessage);
+        }
+    }
+}
---
+++ MauiVirtManager/Tools/Utilities/ServiceProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ServiceProviderExtensions.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiVirtManager.Tools.Utilities
+{
+    /// <summary>
+    /// Service Provider Extensions.
+    /// </summary>
+    public static class ServiceProviderExtensions
+    {
+        public static T ResolveWith<T>(this IServiceProvider provider, params object[] parameters)
+            where T : class =>
+            ActivatorUtilities.CreateInstance<T>(provider, parameters);
+    }
+}
