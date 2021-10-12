379b65619041f232ec1c413a8955e7d9993a94d6
Tim Miller <innerlogic4321@gmail.com>
2021-10-12 15:13:08 -0400

    Add DependencyInjection for Services

--- MauiVirtManager/App.xaml.cs
+++ MauiVirtManager/App.xaml.cs
@@ -1,17 +1,33 @@
-﻿using Microsoft.Maui;
+﻿// <copyright file="App.xaml.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using MauiVirtManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
 using Application = Microsoft.Maui.Controls.Application;
 
 namespace MauiVirtManager
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			InitializeComponent();
+    /// <summary>
+    /// App.
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IServiceProvider services;
+        private readonly INavigationService navigation;
 
-			MainPage = new MainPage();
-		}
-	}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        /// <param name="services">IServiceProvider.</param>
+        public App(IServiceProvider services)
+        {
+            this.services = services;
+            this.navigation = services.GetService<INavigationService>();
+            this.InitializeComponent();
+        }
+    }
 }
--- MauiVirtManager/MauiProgram.cs
+++ MauiVirtManager/MauiProgram.cs
@@ -1,15 +1,30 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls.Compatibility;
+﻿// <copyright file="MauiProgram.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using MauiVirtManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
 using Microsoft.Maui.Hosting;
 
 namespace MauiVirtManager
 {
+    /// <summary>
+    /// Main entry point.
+    /// </summary>
     public static class MauiProgram
     {
+        /// <summary>
+        /// Creates the base MauiApp.
+        /// </summary>
+        /// <returns><see cref="MauiApp"/>.</returns>
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+            builder.Services.AddSingleton<INavigationService, NavigationService>();
+            builder.Services.AddSingleton<IErrorHandlerService, ErrorHandlerService>();
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
